7cd8ceb7d1384874922618451f23321ffee9dca1
Franco Armando <francoarmando1911@gmail.com>
2025-07-24 17:29:59 -0300

    Fix: refactorizacion del main para mostrar datos

--- src/ProgramacionBasica/ArraysYAlgoritmosOrientacion/MainArraysAlgoritmos.java
+++ src/ProgramacionBasica/ArraysYAlgoritmosOrientacion/MainArraysAlgoritmos.java
@@ -3,10 +3,24 @@ package ProgramacionBasica.ArraysYAlgoritmosOrientacion;
 public class MainArraysAlgoritmos {
     public static void main(String[] args) {
         int[] numeros = {1, 7, 9, 3, 4, 30, 20, 55, 86, 75};
+
+        ordenarPorInsercion(numeros);
+        System.out.print("Orden del array por Insercion: ");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        ordenarPorBurbuja(numeros);
+        System.out.print("Orden del array por burbuja: ");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 
     public static void ordenarPorInsercion(int[] array) {
-        for(int i = 1; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             int actual = array[i];
             int j = i - 1;
             while (j >= 0 && array[j] > actual) {
