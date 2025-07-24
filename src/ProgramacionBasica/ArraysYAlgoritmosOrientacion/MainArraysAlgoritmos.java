430d73069e77c0e2bb4707359153e2f851ef03d0
Franco Armando <francoarmando1911@gmail.com>
2025-07-24 17:26:08 -0300

    Feature: ejercicio 2 copmpleto, impementacion de metodos de ordenamiento por insercion y ordenamiento por burbuja

---
+++ src/ProgramacionBasica/ArraysYAlgoritmosOrientacion/MainArraysAlgoritmos.java
@@ -0,0 +1,32 @@
+package ProgramacionBasica.ArraysYAlgoritmosOrientacion;
+
+public class MainArraysAlgoritmos {
+    public static void main(String[] args) {
+        int[] numeros = {1, 7, 9, 3, 4, 30, 20, 55, 86, 75};
+    }
+
+    public static void ordenarPorInsercion(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int actual = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > actual) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = actual;
+        }
+    }
+
+    public static void ordenarPorBurbuja(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
